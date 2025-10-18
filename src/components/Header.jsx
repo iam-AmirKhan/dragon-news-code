1a9af38eee987c513f811ba3d734e92e8b6ae59f
Amir Khan <khansourav119@gmail.com>
2025-10-18 13:39:18 +0600

    2nd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format } from 'date-fns';
 const Header = () => {
     return (
-        <div className='flex justify-center flex-col items-center gap-3'>
-           <img src={logo} alt="" />
-           <p>Journalism Without Fear or Favour</p>
-           <p>Sunday, November 27, 2025</p>
+        <div className='p-6 flex justify-center flex-col items-center gap-3'>
+          
+           <img className='w-[400px]' src={logo} alt="" />
+           
+           <p className='text-accent'>Journalism Without Fear or Favour</p>
+           <p className='text-semibold text-accent'>{format(new Date(),"EEEE, MMMM dd, yyyy hh:mm a")}</p>
         </div>
+        
     );
 };
 
